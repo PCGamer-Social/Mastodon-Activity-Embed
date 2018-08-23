abc8565ae65ac825dba0a777fcd2b1994370a67b
eai04191 <eai@mizle.net>
2018-08-23 11:32:15 +0900

    lint

--- getActivity.js
+++ getActivity.js
@@ -1,25 +1,34 @@
-const domain = decodeURIComponent(window.location.search.match(/domain=(.*?)(&|$)/)[1]);
-const showDomain = Boolean(Number(decodeURIComponent(window.location.search.match(/showDomain=(.*?)(&|$)/)[1])));
+const domain = decodeURIComponent(
+  window.location.search.match(/domain=(.*?)(&|$)/)[1]
+);
+const showDomain = Boolean(
+  Number(
+    decodeURIComponent(window.location.search.match(/showDomain=(.*?)(&|$)/)[1])
+  )
+);
 document.getElementById("domain").innerText = domain;
 if (showDomain) {
-    document.getElementById("domain").className = "";
+  document.getElementById("domain").className = "";
 }
 fetch("https://" + domain + "/api/v1/instance/activity", {
-    method: "GET"
-}).then(function (response) {
+  method: "GET"
+})
+  .then(function(response) {
     if (response.ok) {
-        return response.json();
+      return response.json();
     } else {
-        throw new Error();
+      throw new Error();
     }
-}).then(function (json) {
+  })
+  .then(function(json) {
     let d = new Date(parseInt(json[0]["week"]) * 1000);
     let date = d.getMonth() + 1 + "/" + d.getDate();
     document.getElementById("week").innerText = "(" + date + "~)";
     document.getElementById("toot").innerText = json[0]["statuses"];
     document.getElementById("login").innerText = json[0]["logins"];
     document.getElementById("new").innerText = json[0]["registrations"];
-}).catch(function (error) {
+  })
+  .catch(function(error) {
     document.getElementById("ok").className = "invisible";
     document.getElementById("ng").className = "";
-});
+  });
